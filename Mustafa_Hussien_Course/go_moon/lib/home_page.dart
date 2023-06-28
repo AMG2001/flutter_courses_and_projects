06c08a092a3fadf27dcd50ed9c693a454586e833
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-06-28 08:45:42 +0300

    section 4 finished

--- Mustafa_Hussien_Course/go_moon/lib/home_page.dart
+++ Mustafa_Hussien_Course/go_moon/lib/home_page.dart
@@ -19,62 +19,71 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.all(16),
           width: DeviceDimensions.width,
           height: DeviceDimensions.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Stack(
             children: [
-              Text(
-                "#GoMoon",
-                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              Positioned(
+                top: DeviceDimensions.height * .2,
+                right: 0,
+                child: Image.asset('assets/astro_moon.png'),
               ),
-              Container(
-                child: Column(
-                  children: [
-                    CustomDropDownButton(
-                      initialValue: list1[0],
-                      options: list1,
-                      containerWidth: DeviceDimensions.width,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "#GoMoon",
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    child: Column(
                       children: [
                         CustomDropDownButton(
-                          initialValue: list2[0],
-                          options: list2,
+                          initialValue: list1[0],
+                          options: list1,
+                          containerWidth: DeviceDimensions.width,
                         ),
-                        CustomDropDownButton(
-                          initialValue: list3[0],
-                          options: list3,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CustomDropDownButton(
+                              initialValue: list2[0],
+                              options: list2,
+                            ),
+                            CustomDropDownButton(
+                              initialValue: list3[0],
+                              options: list3,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 18),
+                              backgroundColor: Colors.white),
+                          onPressed: () {},
+                          child: Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 18),
+                            width: DeviceDimensions.width,
+                            child: Text(
+                              "Book Ride!",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          padding:
-                              EdgeInsets.symmetric(vertical: 8, horizontal: 18),
-                          backgroundColor: Colors.white),
-                      onPressed: () {},
-                      child: Container(
-                        alignment: Alignment.center,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 8, horizontal: 18),
-                        width: DeviceDimensions.width,
-                        child: Text(
-                          "Book Ride!",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
+                  )
+                ],
+              ),
             ],
           ),
         ),
