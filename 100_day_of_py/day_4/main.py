99e049284f37dfdc7886dfd07ca058740ea800ff
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-09-16 23:40:26 +0500

    day 4 done

--- 100_day_of_py/day_4/main.py
+++ 100_day_of_py/day_4/main.py
@@ -1,4 +1,3 @@
-import shapes 
 import random
 
 endGame = False
@@ -8,7 +7,6 @@ def userChoice():
     if(inpt==1) : return "rock"
     elif inpt ==2 : return "paper"
     elif inpt == 3 : return "scissors" 
-    # else: userChoice()
 
 def computerChoice():
     choice =  int(random.randint(1,3))
@@ -22,18 +20,52 @@ def checkEndGame():
     if inpt == "y" or inpt == "Y" : endGame = True
     else: endGame = False
 
+def getShape(name_of_shape):
+    if(name_of_shape == "rock"): return '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+    elif(name_of_shape == "paper"): return '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+    else : return '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+def showResult(userChoice, computerChice , result):
+    return f'''
+            user choose : {getShape(userChoice)},
+            computer choose : {getShape(computerChice)}
+            Result : {result} 
+            '''
+
 def checkWinner(userChoice,computerChoice):
-    print(f"user choose : {userChoice} , computer choose : {computerChoice}")
-    if(userChoice == computerChoice) : print("Draw")
+
+    if(userChoice == computerChoice) : print(showResult(userChoice,computerChoice,"Draw"))
     elif(userChoice == "rock"):
-        if(computerChoice == "paper"): print("Computer Win !!")
-        elif(computerChoice == "scissors"):print("User Win !!")
+        if(computerChoice == "paper"): print(showResult(userChoice,computerChoice,"Computer Win !!"))
+        elif(computerChoice == "scissors"):print(showResult(userChoice,computerChoice,"User Win !!"))
     elif(userChoice == "paper"):
-        if(computerChoice == "rock"):print("User Win !!")
-        elif(computerChoice == "scissors"):print("Computer Win !!")
+        if(computerChoice == "rock"):print(showResult(userChoice,computerChoice,"User Win !!"))
+        elif(computerChoice == "scissors"):print(showResult(userChoice,computerChoice,"Computer Win !!"))
     elif(userChoice == "scissors"):
-        if(computerChoice == "rock"):print("Computer Win !!")
-        elif(computerChoice == "paper") : print("User Win !!")
+        if(computerChoice == "rock"):print(showResult(userChoice,computerChoice,"Computer Win !!"))
+        elif(computerChoice == "paper") : print(showResult(userChoice,computerChoice,"User Win !!"))
     checkEndGame()
 
 while(not endGame):
